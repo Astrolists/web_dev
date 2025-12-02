2ef52bb480675071ee0f183df658f3648dcb3cbd
Astrolists <xander.parker@ucas-edu.net>
2025-12-02 14:39:39 -0700

    STUFF

---
+++ notes_flask/tutorial.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST", "GET"])
+
+def home():
+    if request.method == "POST":
+        return redirect(url_for("user", name=request.form("name")))
+
+    return render_template("index.html")
+
+@app.route("/contact")
+def contact():
+    return "<p>Who are you</p"
+
+@app.route("/<name>")
+def user(name):
+    return f"<p>hello {name}!<?p"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+#What does Flask do?
+    #Gives access to multi-page websites.
+
+
+#What are the steps to setting up a Flask project?
+#How can you reference subpages on your Flask project? (Meaning the difference between the home page and a personal profile)
+#What are templates?
